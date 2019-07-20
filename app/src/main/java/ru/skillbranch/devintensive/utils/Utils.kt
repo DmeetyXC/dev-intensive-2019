0bf45b81f21b5366b94ae388602e7c9bd0d55f22
Dmitriy Bakunovich <dmitriy.bakunovich@gmail.com>
2019-07-20 23:59:54 +0300

    Completed task 3

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,13 +1,22 @@
 package ru.skillbranch.devintensive.utils
 
+import android.app.Activity
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+
 object Utils {
 
     private val translitMap = mapOf(
-            'а' to "a", 'б' to "b", 'в' to "v", 'г' to "g", 'д' to "d", 'е' to "e", 'ё' to "e",
-            'ж' to "zh", 'з' to "z", 'и' to "i", 'й' to "i", 'к' to "k", 'л' to "l", 'м' to "m",
-            'н' to "n", 'о' to "o", 'п' to "p", 'р' to "r", 'с' to "s", 'т' to "t", 'у' to "u",
-            'ф' to "f", 'х' to "h", 'ц' to "c", 'ч' to "ch", 'ш' to "sh", 'щ' to "sh",
-            'ъ' to "", 'ы' to "i", 'ь' to "", 'э' to "e", 'ю' to "yu", 'я' to "ya"
+        'а' to "a", 'б' to "b", 'в' to "v", 'г' to "g", 'д' to "d", 'е' to "e", 'ё' to "e",
+        'ж' to "zh", 'з' to "z", 'и' to "i", 'й' to "i", 'к' to "k", 'л' to "l", 'м' to "m",
+        'н' to "n", 'о' to "o", 'п' to "p", 'р' to "r", 'с' to "s", 'т' to "t", 'у' to "u",
+        'ф' to "f", 'х' to "h", 'ц' to "c", 'ч' to "ch", 'ш' to "sh", 'щ' to "sh",
+        'ъ' to "", 'ы' to "i", 'ь' to "", 'э' to "e", 'ю' to "yu", 'я' to "ya"
     )
 
     fun parseFullName(fullName: String?): Pair<String?, String?> {
@@ -35,12 +44,12 @@ object Utils {
     fun transliteration(payload: String, divider: String = " ") = buildString {
         payload.forEach {
             append(
-                    when {
-                        it == ' ' -> divider
-                        it.isUpperCase() -> translitMap[it.toLowerCase()]?.capitalize()
-                                ?: it.toString()
-                        else -> translitMap[it] ?: it.toString()
-                    }
+                when {
+                    it == ' ' -> divider
+                    it.isUpperCase() -> translitMap[it.toLowerCase()]?.capitalize()
+                        ?: it.toString()
+                    else -> translitMap[it] ?: it.toString()
+                }
             )
         }
     }
@@ -54,5 +63,32 @@ object Utils {
         !firstName.isNullOrBlank() && !lastName.isNullOrBlank() ->
             firstName[0].toUpperCase() + lastName[0].toUpperCase().toString()
         else -> throw IllegalStateException("Incorrect state in 'when' expression")
+    }
+
+    fun Activity.hideKeyboard() {
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+    }
+
+    fun Activity.getRootView(): View {
+        return findViewById<View>(android.R.id.content)
+    }
+
+    fun Context.convertDpToPx(dp: Float): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, dp, this.resources.displayMetrics
+        )
+    }
+
+    fun Activity.isKeyboardOpen(): Boolean {
+        val visibleBounds = Rect()
+        this.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
+        val heightDiff = getRootView().height - visibleBounds.height()
+        val marginOfError = Math.round(this.convertDpToPx(50F))
+        return heightDiff > marginOfError
+    }
+
+    fun Activity.isKeyboardClosed(): Boolean {
+        return !this.isKeyboardOpen()
     }
 }
